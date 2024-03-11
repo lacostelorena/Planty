d985c01ae119afe21c3eafec1b53fa98341c8fa8
lacoste lorena <lacoste.lorena@gmail.com>
2024-03-11 15:39:45 +0100

    nouvelles modifications

--- wp-content/themes/planty/functions.php
+++ wp-content/themes/planty/functions.php
@@ -3,13 +3,12 @@
 add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
 function theme_enqueue_styles()
 {
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style('parent-style', get_stylesheet_directory_uri() . '/style.css');
 }
 
 add_filter('wp_nav_menu_items','Ajout_adminlink', 10, 2);
-function Ajout_adminlink($items, $args)
-{
-    if(is_user_logged_in() && $args->theme_location == 'nav-menu'){
+function Ajout_adminlink($items, $args){
+    if(is_user_logged_in() && $args->theme_location == 'primary'){
         $items .='<li><a href="'.get_admin_url().'">Admin</a></li>';
     }
     return $items;
